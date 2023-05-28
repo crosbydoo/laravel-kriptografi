00b28333e24f661be199cfe1c37e73fc98be57ee
Crosby <xxristuaji141@gmail.com>
2023-05-28 10:49:06 +0700

    upload to github

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Dompdf\Dompdf;
+use App\Models\File;
+use Illuminate\Http\Request;
+use Smalot\PdfParser\Parser;
+use Illuminate\Support\Facades\Crypt as AES;
+use Illuminate\Support\Facades\Storage;
+
+class FileController extends Controller
+{
+    //
+    public function index()
+    {
+        $indexes = File::all(); // Ganti 'Index' dengan model yang sesuai
+
+        return view('encrypt.index', compact('indexes'));
+    }
+
+    //upload file
+
+    public function store(Request $request)
+    {
+        $file = $request->file;
+
+        $request->validate([
+            'file' => 'required|mimes:pdf',
+        ]);
+
+        // Gunakan pdf parser untuk membaca konten dari file pdf
+        $fileName = $file->getClientOriginalName();
+
+        $pdfParser = new Parser();
+        $pdf = $pdfParser->parseFile($file->path());
+        $content = $pdf->getText();
+
+        // Enkripsi konten menggunakan AES
+        $encryptedContent = AES::encrypt($content);
+
+        // Mendapatkan angka auto-increment berikutnya
+        $nextIncrement = File::count() + 1;
+
+        // Ubah nama file menjadi "encrypt.pdf" dengan penambahan angka auto-increment
+        $newFileName = "encrypt{$nextIncrement}.pdf";
+
+        // Simpan file di storage dengan nama baru
+        $path = $file->storeAs('pdf_files', $newFileName, 'public');
+
+        // Buat objek Dompdf
+        $dompdf = new Dompdf();
+
+        // Render konten terenkripsi sebagai HTML
+        $dompdf->loadHtml($encryptedContent);
+
+        // Render HTML menjadi file PDF
+        $dompdf->render();
+
+        // Simpan hasil render sebagai file PDF
+        $newPdfFilePath = 'pdf_files/' . $newFileName;
+        Storage::disk('public')->put($newPdfFilePath, $dompdf->output());
+
+        $upload_file = new File;
+        $upload_file->orig_filename = $fileName;
+        $upload_file->mime_type = $file->getMimeType();
+        $upload_file->filesize = $file->getSize();
+        $upload_file->content = $encryptedContent;
+        $upload_file->file_path = Storage::disk('public')->url($newPdfFilePath); // Simpan URL file di dalam kolom "file_path"
+        $upload_file->save();
+
+        return redirect()->back()->with('success', 'File submitted');
+    }
+}
---
+++ resources/views/encrypt/index.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Encrypt') }}
+        </h2>
+    </x-slot>
+
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __('Upload to Encrypt PDF File') }}
+
+                    <form method="POST" action="{{ route('file.store') }}" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="mb-4">
+                            <label for="pdf" class="block text-gray-700 font-bold mb-2">Select PDF File</label>
+                            <input type="file" id="pdf" name="file" accept=".pdf"
+                                class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        </div>
+
+                        <button type="submit"
+                            class="bg-cyan-600 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500">
+                            {{ __('Upload') }}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
